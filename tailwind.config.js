d2f7218c5a2bf9e2ac09edc5a8939a19550f5e73
ivlopez03 <ivlopez.dev@gmail.com>
2024-05-26 23:45:09 -0700

    Initialized vite project

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "mainBackgroundColor": "#0D1117"
+      }
+    },
+  },
+  plugins: [],
+}
+
